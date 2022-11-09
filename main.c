3856144ee681fa6629b376047cf218879702b152
JRamonMartinez <117685632+JRamonMartinez@users.noreply.github.com>
2022-11-08 21:27:36 -0600

    Initial commit
    25 pair color code

--- main.c
+++ main.c
@@ -1,78 +1,71 @@
 #include <stdio.h>
 #include <assert.h>
+#include "main.h"
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
+static void ColorPairToString(const ColorPair* colorPair, char* buffer) 
+{
+    sprintf(buffer, "%s and %s",
+    MajorColorNames[colorPair->majorColor],
+    MinorColorNames[colorPair->minorColor]);
 }
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
+static ColorPair GetColorFromPairNumber(int pairNumber) 
+{
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =(enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+
     return colorPair;
 }
 
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
-
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
+static int GetPairNumberFromColor(const ColorPair* colorPair) 
 {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
 }
 
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
 {
     ColorPair colorPair;
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
     int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
+    printf("Pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
 
-int main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Correspond to twisted pairs %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
 
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
+int main() 
+{
+    int majorColoridx;
+    int minorColoridx;
+    static int offset = 1;
+    FILE *fp;
+
+    fp = freopen("25Pair_color_code.txt", "w+", stdout);
+    if(fp == NULL)
+    {
+        return 0;
+    }
+    for (majorColoridx = 0; majorColoridx < 5; majorColoridx++) 
+    {
+		for (minorColoridx = 0; minorColoridx < 5; minorColoridx++) 
+        {
+            testPairToNumber(majorColoridx, minorColoridx, (minorColoridx + offset));
+            testNumberToPair((minorColoridx + offset), majorColoridx, minorColoridx);
+		}
+		offset += 5;
+	}
+    fclose(fp);
 
     return 0;
 }
